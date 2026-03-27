a15202ccdfdfee73ae4e9fbef7fe3aae39e1fe25
Stevens Herrera <steveherrera98@hotmail.com>
2026-03-27 15:13:12 -0500

    se termina la semana 3

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from src.inventario import agregar_producto,ver_inventario,eliminar_producto,editar_producto,calcular_estadisticas
-# Menú principal
+from src.inventario import (
+    agregar_producto, ver_inventario, eliminar_producto,
+    calcular_estadisticas,editar_producto,
+    buscar_en_menu,ejecutar_carga_interactiva,guardar_datos
+)
+# ==============================
+# MENÚ PRINCIPAL
+# ==============================
 
 while True:
 
@@ -7,9 +13,12 @@ while True:
     print("1. Agregar producto.")
     print("2. Ver inventario.")
     print("3. Editar producto.")
-    print("4. Eliminar producto.")
-    print("5. Total productos.")
-    print("6. Salir.")
+    print("4. Buscar producto.")
+    print("5. Eliminar producto.")
+    print("6. Total productos.")
+    print("7. Guardar CSV.")
+    print("8. Cargar CSV.")
+    print("9. Salir.")
 
     opcion = input("Seleccione una opción: ")
 
@@ -21,14 +30,29 @@ while True:
 
     elif opcion == "3":
         editar_producto()
-
+    
     elif opcion == "4":
-        eliminar_producto()    
+        buscar_en_menu()
 
     elif opcion == "5":
-        calcular_estadisticas()    
+        eliminar_producto()    
 
     elif opcion == "6":
+        calcular_estadisticas()
+
+    elif opcion == "7": 
+            guardar_datos()
+
+    elif opcion == "8": 
+            ejecutar_carga_interactiva()    
+
+    elif opcion == "9":
+        confirmacion = input("¿Desea guardar los cambios en el CSV antes de salir? (S/N): ").upper()
+        
+        if confirmacion == "S":
+            guardar_datos() # Llama a la función que ya definimos en src/inventario
+            print("Datos guardados correctamente.")
+            
         print("Programa finalizado.")
         break
 
